90b5824ae1c48fa4385a078eb9705092105437ed
xscapefromme <markospineda585@gmail.com>
2025-10-28 13:35:37 -0700

    toggle working when user clicks shareBtn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+let isOn = false;
+const card = document.querySelector('.centerCard');
+const shareBtn = document.getElementById('shareIcon');
+const shareContainer = document.createElement('div');
+shareBtn.addEventListener('click', function () {
+  isOn = !isOn; //inverts the boolean value when user clicks
+  if (isOn) {
+    renderIconItems();
+  } else {
+    removeIconItems();
+  }
 
+  console.log(isOn);
+});
+
+function renderIconItems() {
+  shareContainer.innerHTML = `<p>share</p> 
+  <span>
+  <img src="images/icon-facebook.svg"><img src="images/icon-twitter.svg"><img src="images/icon-pinterest.svg"></span>`;
+  shareContainer.classList.add('shareContainerOnClick');
+  card.append(shareContainer);
+}
+
+function removeIconItems() {
+  shareContainer.remove();
+}
